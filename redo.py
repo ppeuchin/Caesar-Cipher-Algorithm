9f87c504aa3f433513f0387e792af332f99799a5
Deandra <akualisel@gmail.com>
2021-06-03 22:17:40 +0000

    added extra functionality

--- redo.py
+++ redo.py
@@ -1,1 +1,17 @@
-print('hi')
+import time
+from option_validator import options
+import os
+
+def redo():
+    os.system('cls')
+    print('*******************************')
+    print('What would you like to do next?')
+    time.sleep(1)
+    print('\n1. Encrypt another')
+    print('2. Decrypt another')
+    print('3. Exit\n')
+    time.sleep(1)
+    options()
+    print('\nPress any key to continue')
+    input()
+    redo()
